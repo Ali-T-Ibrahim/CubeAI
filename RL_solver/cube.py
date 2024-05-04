5f2b3c5194d16c5ce9867920402cd30906f69288
Ali <ali--ibrahim@hotmail.com>
2024-05-04 15:43:51 -0400

    Complete move methods for manipulating cube

--- RL_solver/cube.py
+++ RL_solver/cube.py
@@ -24,41 +24,124 @@ class Cube:
                                                                           self.face['R'],
                                                                           self.face['L'])
     def u_move(self):
+        # matrix manipulation
+        temp = self.face['F'][0].copy()
+        self.face['F'][0] = self.face['R'][0]
+        self.face['R'][0] = self.face['B'][0]
+        self.face['B'][0] = self.face['L'][0]
+        self.face['L'][0] = temp
+        # logging
         self.move = "U"
 
     def up_move(self):
+        # matrix manipulation
+        temp = self.face['F'][0].copy()
+        self.face['F'][0] = self.face['L'][0]
+        self.face['L'][0] = self.face['B'][0]
+        self.face['B'][0] = self.face['R'][0]
+        self.face['R'][0] = temp
+        # logging
         self.move = "U'"
 
     def d_move(self):
+        # matrix manipulation
+        temp = self.face['F'][2].copy()
+        self.face['F'][2] = self.face['L'][2]
+        self.face['L'][2] = self.face['B'][2]
+        self.face['B'][2] = self.face['R'][2]
+        self.face['R'][2] = temp
+        # logging
         self.move = "D"
 
     def dp_move(self):
+        # matrix manipulation
+        temp = self.face['F'][2].copy()
+        self.face['F'][2] = self.face['R'][2]
+        self.face['R'][2] = self.face['B'][2]
+        self.face['B'][2] = self.face['L'][2]
+        self.face['L'][2] = temp
+        # logging
         self.move = "D'"
 
     def f_move(self):
+        # matrix manipulation
+        temp = self.face['U'][2].copy()
+        self.face['U'][2] = self.face['L'][:, 2]
+        self.face['L'][:, 2] = self.face['D'][0]
+        self.face['D'][0] = self.face['R'][:, 0]
+        self.face['R'][:, 0] = temp
+        # logging
         self.move = "F"
 
     def fp_move(self):
+        # matrix manipulation
+        temp = self.face['U'][2].copy()
+        self.face['U'][2] = self.face['R'][:, 0]
+        self.face['R'][:, 0] = self.face['D'][0]
+        self.face['D'][0] = self.face['L'][:, 2]
+        self.face['L'][:, 2] = temp
+        # logging
         self.move = "F'"
-
     def b_move(self):
+        # matrix manipulation
+        temp = self.face['U'][0].copy()
+        self.face['U'][0] = self.face['R'][:, 2]
+        self.face['R'][:, 2] = self.face['D'][2]
+        self.face['D'][2] = self.face['L'][:, 0]
+        self.face['L'][:, 0] = temp
+        # logging
         self.move = "B"
 
     def bp_move(self):
+        # matrix manipulation
+        temp = self.face['U'][0].copy()
+        self.face['U'][0] = self.face['L'][:, 0]
+        self.face['L'][:, 0] = self.face['D'][2]
+        self.face['D'][2] = self.face['R'][:, 2]
+        self.face['R'][:, 2] = temp
+        # logging
         self.move = "B'"
 
     def r_move(self):
+        # matrix manipulation
+        temp = self.face['U'][:, [2]].copy()
+        self.face['U'][:, [2]] = self.face['F'][:, [2]]
+        self.face['F'][:, [2]] = self.face['D'][:, [2]]
+        self.face['D'][:, [2]] = self.face['B'][:, [2]]
+        self.face['B'][:, [2]] = temp
+        # logging
         self.move = "R"
 
     def rp_move(self):
+        # matrix manipulation
+        temp = self.face['U'][:, [2]].copy()
+        self.face['U'][:, [2]] = self.face['B'][:, [2]]
+        self.face['B'][:, [2]] = self.face['D'][:, [2]]
+        self.face['D'][:, [2]] = self.face['F'][:, [2]]
+        self.face['F'][:, [2]] = temp
+        # logging
         self.move = "R'"
 
     def l_move(self):
+        # matrix manipulation
+        temp = self.face['U'][:, [0]].copy()
+        self.face['U'][:, [0]] = self.face['B'][:, [0]]
+        self.face['B'][:, [0]] = self.face['D'][:, [0]]
+        self.face['D'][:, [0]] = self.face['F'][:, [0]]
+        self.face['F'][:, [0]] = temp
+        # logging
         self.move = "L"
 
     def lp_move(self):
+        # matrix manipulation
+        # matrix manipulation
+        temp = self.face['U'][:, [0]].copy()
+        self.face['U'][:, [0]] = self.face['F'][:, [0]]
+        self.face['F'][:, [0]] = self.face['D'][:, [0]]
+        self.face['D'][:, [0]] = self.face['B'][:, [0]]
+        self.face['B'][:, [0]] = temp
+        # logging
         self.move = "L'"
-
 
 
 
@@ -68,6 +151,7 @@ class Cube:
 
 
 cube = Cube()
-cube.visualize_string()
 cube.z2()
+cube.visualize_string()
+cube.u_move()
 cube.visualize_string()
